27648bd828e7ab1280ec46d1689fa74e31b2db33
Josh Ourisman <josh@joshourisman.com>
2010-02-13 21:35:38 -0500

    added yasnippet-0.6.1c to plugins

--- init.el
+++ init.el
@@ -5,3 +5,16 @@
 
 ;; auto-indent new lines
 (define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; pyflakes
+(when (load "flymake" t) 
+  (defun flymake-pyflakes-init () 
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                       'flymake-create-temp-inplace)) 
+           (local-file (file-relative-name 
+                        temp-file 
+                        (file-name-directory buffer-file-name)))) 
+      (list "pyflakes" (list local-file)))) 
+  (add-to-list 'flymake-allowed-file-name-masks 
+               '("\\.py\\'" flymake-pyflakes-init)))
+(add-hook 'python-mode-hook 'flymake-mode)
