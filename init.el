ab899208e7bf479a398b926ba148691b95d5053f
Josh Ourisman <josh@joshourisman.com>
2010-03-14 15:13:58 -0400

    turned off flymake-mode; still fucked up

--- init.el
+++ init.el
@@ -114,17 +114,17 @@
 
 ;; code checking via flymake
 ;; set code checker here from "epylint", "pyflakes"
-(setq pycodechecker "pyflakes")
-(when (load "flymake" t)
-  (defun flymake-pycodecheck-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list pycodechecker (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pycodecheck-init)))
+;; (setq pycodechecker "pyflakes")
+;; (when (load "flymake" t)
+;;   (defun flymake-pycodecheck-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name
+;;                         temp-file
+;;                         (file-name-directory buffer-file-name))))
+;;       (list pycodechecker (list local-file))))
+;;   (add-to-list 'flymake-allowed-file-name-masks
+;;                '("\\.py\\'" flymake-pycodecheck-init)))
 
-(add-hook 'python-mode-hook 'flymake-mode)
+;; (add-hook 'python-mode-hook 'flymake-mode)
 
