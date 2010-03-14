36af5c922400666e2c794d4e1bdac4a19383c3c4
Josh Ourisman <josh@joshourisman.com>
2010-03-14 15:13:40 -0400

    Using textmate-mode for HTML and Text mode as well.

--- init.el
+++ init.el
@@ -7,7 +7,9 @@
 ;; set up textmate-mode
 (require 'textmate-mode)
 (add-hook 'python-mode-hook 'textmate-mode)
+(add-hook 'html-mode-hook 'textmate-mode)
 (add-hook 'emacs-lisp-mode-hook 'textmate-mode)
+(add-hook 'text-mode-hook 'textmate-mode)
 
 ;; set up electric pairs for elisp-mode
 ;; (add-hook 'emacs-lisp-mode-hook
